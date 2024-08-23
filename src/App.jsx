1109b284c50d05972fc8c86cea4f24a3ff942ba8
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2024-08-24 01:24:01 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import About from "./pages/About";
